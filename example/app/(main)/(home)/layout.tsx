e35f0b800275c9afb663ea30c07a0c51c6d3e749
plrs9816 <61955474+plrs9816@users.noreply.github.com>
2023-08-03 20:46:37 +0900

    fix: docs home

--- example/app/(main)/(home)/layout.tsx
+++ example/app/(main)/(home)/layout.tsx
@@ -1,15 +1,11 @@
 import { Nav } from "@/components/LinkItem";
 
-function Layout({ children }: { children?: React.ReactNode }) {
-  console.log(children === null);
+function Layout() {
   return (
     <div className="md:bg-gray-50">
       <div className="container max-w-6xl min-h-screen flex flex-col">
         <div className="md:grid md:grid-cols-[auto_1fr] md:gap-12 flex-1 md:py-32">
           <Nav />
-          <div className="relative md:border overflow-hidden bg-white max-md:fixed inset-0">
-            {children}
-          </div>
         </div>
       </div>
     </div>
--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      height: {
+        screen: ["100vh", "100dvh"],
+      },
+      minHeight: {
+        screen: ["100vh", "100dvh"]
+      },
+      maxHeight: {
+        screen: ["100vh", "100dvh"]
+      },
       container: {
         center: true,
       },
